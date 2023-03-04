f2fc20abb87292e8d5c60b0b3430a4b7a08027c8
trupier <michal.konkolewski01158@gmail.com>
2023-03-04 15:08:14 +0100

    in proggress. I am not creative at labeling commits xD

--- buyerapp/shoplist/models.py
+++ buyerapp/shoplist/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Shoplist(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=200, default='Shopping List')
+    #user_id = models.IntegerField()
+
+class Item(models.Model):
+    shoplist = models.ForeignKey(Shoplist, on_delete=models.CASCADE)
+    item_name = models.CharField(max_length=200)
+    is_taken = models.BooleanField(default=False)
+
--- buyerapp/shoplist/views.py
+++ buyerapp/shoplist/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Shoplist, Item
 # Create your views here.
+def index(request, ):
+    pass
+
+def showlist(request):
+    pass
+
